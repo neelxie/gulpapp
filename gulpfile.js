d6f930528e8cd1189f9bb75bd5a19913a7fd9287
neelxie <kidricederek@gmail.com>
2021-10-17 17:38:24 +0300

    Set up gulp project

---
+++ gulpfile.js
@@ -0,0 +1,15 @@
+const gulp = require("gulp");
+
+/*
+  -- TOP LEVEL FUNCTIONS --
+  gulp.task - Define tasks
+  gulp.src - Point to files to use
+  gulp.dest - Points to folder to output
+  gulp.watch - Watch files and folders for changes
+*/
+
+// log a message
+
+gulp.task("message", function () {
+  return console.log("Gulp is running...");
+});
